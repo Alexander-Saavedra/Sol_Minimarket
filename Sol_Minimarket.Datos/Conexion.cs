0a7fa0b334928d2433333b5cb91b22643b8eb30c
Alexander Saavedra <alexsaavedra718@gmail.com>
2025-04-16 17:01:19 -0600

    Conexion con la base de datos

--- Sol_Minimarket.Datos/Conexion.cs
+++ Sol_Minimarket.Datos/Conexion.cs
@@ -9,5 +9,52 @@ namespace Sol_Minimarket.Datos
 {
     public class Conexion
     {
+        private string Base;
+        private string Servidor;
+        private string Usuario;
+        private string Clave;
+        private bool Seguridad;
+        private static Conexion Con = null;
+
+        private Conexion()
+        {
+            this.Base = "BD_Minimarket";
+            this.Servidor = "DESKTOP-KIDS6OC\\SOLUSYSTEMS";
+            this.Usuario = "Sistemas";
+            this.Clave = "Solusystems2024*.";
+            this.Seguridad = false;
+        }
+
+        public SqlConnection CrearConexion()
+        {
+            SqlConnection Cadena = new SqlConnection();
+            try
+            {
+                Cadena.ConnectionString = "Server=" + this.Servidor + "Database=" + this.Base + ";";
+                if (this.Seguridad)
+                {
+                    Cadena.ConnectionString = Cadena.ConnectionString + "Integrated Security = SSPI";
+                }
+                else 
+                {
+                    Cadena.ConnectionString = Cadena.ConnectionString + "User ID="+this.Usuario+"; Password=" + this.Clave;
+                }
+            }
+            catch(Exception ex) 
+            {
+                Cadena = null;
+                throw ex;
+            }
+            return Cadena;
+        }
+
+        public static Conexion getInsstancia()
+        {
+            if (Con == null)
+            {
+                Con = new Conexion();
+            }
+            return Con;
+        }
     }
 }
